b25f8ad07dda9c5551a94f3e23c91d26d1f57b0b
Luke Howe <lahowe@email.sc.edu>
2023-11-06 15:02:44 -0500

    added firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyD1kJ0pLdD_Qn2ElFBD_ZxbTrfFdV6pjdg",
+
+  authDomain: "iiq-trashcan-94e32.firebaseapp.com",
+
+  projectId: "iiq-trashcan-94e32",
+
+  storageBucket: "iiq-trashcan-94e32.appspot.com",
+
+  messagingSenderId: "400283467133",
+
+  appId: "1:400283467133:web:b8d99ba5fa320cffc6a014"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
