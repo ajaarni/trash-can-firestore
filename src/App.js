8d1d8214c32302e510793b835f47e9aaeee518fd
ajaarni <aaarnink@email.sc.edu>
2023-11-06 16:31:19 -0500

    works still/vid link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc } from "firebase/firestore";
 import { useState, useEffect } from "react";
 import { db } from "./firebase";
 
 function App() {
-  const [inventorys, setInventory] = useState([]);
+  const [newItemName, setNewItemName] = useState("");
+  const [newMaker, setNewMaker] = useState("");
+  const [newQuantity, setQuantity] = useState(0);
+
+  const [inventory, setInventory] = useState([]);
   const inventoryRef = collection(db, "inventory");
+
+  const createItem = async () => {
+    await addDoc(inventoryRef, {
+      itemname: newItemName,
+      manufacturer: newMaker,
+      quantity: newQuantity,
+    });
+  };
 
   useEffect(() => {
     const getInventory = async () => {
@@ -17,7 +29,31 @@ function App() {
 
   return (
     <div className="App">
-      {inventorys.map((inventory) => {
+      <input
+        placeholder="Item Name"
+        onChange={(e) => {
+          setNewItemName(e.target.value);
+        }}
+      />
+      <input
+        placeholder="Manufacturer"
+        onChange={(e) => {
+          setNewMaker(e.target.value);
+        }}
+      />
+      <input
+        type="number"
+        placeholder="Quantity"
+        onChange={(e) => {
+          setQuantity(e.target.value);
+        }}
+      />
+
+      <button type="submit" onClick={createItem}>
+        Add Inventory
+      </button>
+
+      {inventory.map((inventory) => {
         return (
           <div>
             <h1> Item: {inventory.itemname}</h1>
